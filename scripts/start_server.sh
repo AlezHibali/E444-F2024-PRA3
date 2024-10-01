93e33c0369405d90b964eecdd7e3bb67507cdb22
AlezHibali <ali.daixin.tian@gmail.com>
2024-10-01 04:30:22 -0400

    test deploy

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/E444-F2024-PRA3/flaskr-tdd/
-sudo nohup python3 -m waitress --host=0.0.0.0 --port=5001 project.app:app &
+sudo nohup python3 -m waitress --host=0.0.0.0 --port=5001 project.app:app > /path/to/your/logfile.log 2>&1 &
