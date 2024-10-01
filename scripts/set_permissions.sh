bea72a866e770053f296d54f256854a68c2083c6
AlezHibali <ali.daixin.tian@gmail.com>
2024-10-01 04:02:08 -0400

    test deploy

---
+++ scripts/set_permissions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo chmod +x /home/ec2-user/E444-F2024-PRA3/scripts/after_install.sh
+sudo chmod +x /home/ec2-user/E444-F2024-PRA3/scripts/before_install.sh
+sudo chmod +x /home/ec2-user/E444-F2024-PRA3/scripts/start_server.sh
+sudo chmod +x /home/ec2-user/E444-F2024-PRA3/scripts/stop_server.sh
+sudo chmod +x "$0"
