68372fd7adbe654a24a36015bd6df50ae44cf54e
AlezHibali <ali.daixin.tian@gmail.com>
2024-10-01 03:30:42 -0400

    fix host to be externally accessible; fix cmd to stop the server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/E444-F2024-PRA3/flaskr-tdd/
-sudo nohup python3 -m flask --app project/app.py run -p 5001 &
+sudo nohup python3 -m flask --app project/app.py run -p 5001 --host=0.0.0.0 &
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pkill -f "flask run"
+sudo pkill -f "flask"
