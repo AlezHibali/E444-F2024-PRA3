b0f45d395b7ae567aa98f90e046920ee00801f5c
AlezHibali <ali.daixin.tian@gmail.com>
2024-10-03 17:04:48 -0400

    add test_search in app_test

--- flaskr-tdd/tests/app_test.py
+++ flaskr-tdd/tests/app_test.py
@@ -86,3 +86,26 @@ def test_delete_message(client):
     rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+
+def test_search(client):
+    """Ensure the search functions works correctly"""
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    post_rv = client.post(
+        "/add",
+        data=dict(title="<Hello>", text="<strong>HTML</strong> allowed here"),
+        follow_redirects=True,
+    )
+    rv = client.get(
+        "/search/?query=Hello",
+        follow_redirects=True,
+    )
+    assert rv.status_code == 200
+    assert b"Hello" in rv.data
+    assert b"<strong>HTML</strong> allowed here" in rv.data
+
+    rv = client.get(
+        "/search/?query=Daixin",
+        follow_redirects=True,
+    )
+    assert rv.status_code == 200
